ef0c27e068d6b1a484b4c9fc9b3ca29b3489fdcf
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-27 01:05:37 +0500

    fix: remove winners test data

--- src/store/winnersStore.ts
+++ src/store/winnersStore.ts
@@ -1,8 +1,3 @@
 import { UserId, Winner } from './types';
 
-export const winnersStore = new Map<UserId, Winner>([
-  ['test', { name: 'test 1', wins: 1 }],
-  ['test1', { name: 'test 2', wins: 5 }],
-  ['test2', { name: 'test 3', wins: 3 }],
-  ['test3', { name: 'test 4', wins: 2 }],
-]);
+export const winnersStore = new Map<UserId, Winner>();
